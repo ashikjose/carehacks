4d03c16da970a3229ae25d1b6b4a2694d4fc858a
Ashik <ajashik@outlook.com>
2017-11-14 21:27:10 +0530

    Initial UI Code

---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row">
+            <div class="container-fluid">
+              <div class="left">
+                <p class="white lato">View Bookings</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="row">
+            <div class="container-fluid">
+              <div class="right">
+                <p class="lato white">Book</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+p{
+  position: relative;
+  vertical-align: middle;
+  text-align: center;
+  top: 45%;
+}
+
+.container-fluid{
+  margin-left: -15px;
+  margin-right: -15px;
+}
+.lato{
+  font-family: 'Lato', sans-serif;
+  font-weight: 300;
+  font-size: 30px;
+}
+
+.mad-full-height{
+  height: 100vh;
+  widows: 100vw;
+  display: block;
+}
+
+.white{
+  color: white;
+  vertical-align: middle;
+  text-align: center;
+}
+
+.left{
+  height: 100vh;
+  background-color: #3b2577;
+  display: block;
+  cursor: pointer;
+}
+
+.left:hover
+{
+  background-color: #ffcc00;
+}
+.left:hover > p {
+  color: #3b2577;
+}
+
+.right{
+  height: 100vh;
+  background-color: #3b2577;
+  display: block;
+  cursor: pointer;
+}
+.right:hover {
+  background-color: #ffcc00;
+}
+.right:hover > p {
+  color: #3b2577;
+}
+
+</style>
---
+++ src/components/Hospitals.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        
+            <div class="container-fluid fluidblock">
+                <div class="row imp-padding">
+                    <form>
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <FormInput v-bind:type="textOne" v-bind:name="nameOne" v-bind:text="text"></FormInput>
+                            <div class="mb">
+                                <MadButton v-bind:msg="Msg"></MadButton>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        
+
+    </div>
+</template>
+
+<script>
+import MadButton from '@/components/MadButton'
+import FormInput from '@/components/FormInputJump'
+export default {
+  name: 'Hospital',
+  components: {
+    MadButton,
+    FormInput
+  },
+  data () {
+    return {
+      textOne: 'text',
+      nameOne: 'search',
+      Msg: 'Search',
+      text: 'Enter Hospital\'s name or Doctor\'s name'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.fluidblock{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.imp-padding{
+    padding-top: 25px;
+    
+}
+
+.mb {
+    display: inline-block;
+    position: absolute;
+    right: 15px;
+    top: 20px;
+}
+
+</style>
